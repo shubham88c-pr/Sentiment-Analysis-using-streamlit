237f2fc5f3500a763ca2c6a3b383361052cf4bf2
shubhamsharma-Destroyer <79332660+shubham88c-pr@users.noreply.github.com>
2024-04-18 19:30:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 # Load the pre-trained model
-# with open('sentiment_model.pickle', 'rb') as f:
-#     model = pickle.load(f)
-with open('sentiment_model.pickle', 'wb') as f:
-    pickle.dump(model, f)
+with open('sentiment_model.pickle', 'rb') as f:
+    model = pickle.load(f)
 
 # Load the tokenizer for the model
 tokenizer = AutoTokenizer.from_pretrained("FacebookAI/roberta-base")
