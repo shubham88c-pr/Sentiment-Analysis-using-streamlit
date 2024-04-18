9f3101d465cb10628510ff8db3a9877c2b6589b6
shubhamsharma-Destroyer <79332660+shubham88c-pr@users.noreply.github.com>
2024-04-18 19:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 # Load the pre-trained model
-with open('sentiment_model.pickle', 'rb') as f:
-    model = pickle.load(f)
+# with open('sentiment_model.pickle', 'rb') as f:
+#     model = pickle.load(f)
 with open('sentiment_model.pickle', 'wb') as f:
     pickle.dump(model, f)
 
